6a8e865c509581f7fa66cfce5901b3094f36ee19
joannacfc <chang111@msu.edu>
2024-04-20 16:41:40 -0400

    responsive UI - MUI

--- src/About.js
+++ src/About.js
@@ -1,15 +1,23 @@
-import './About.css';
+import Grid from '@mui/material/Grid';
 
 function About() {
   const headshotUrl = 'https://xftjoyrggcewuljctysj.supabase.co/storage/v1/object/public/headshot/headshot.png';
 
   return (
-    <div className="About">
-      {headshotUrl && <img src={headshotUrl} alt="Joanna Chang" className="headshot" />}
-      <div className="text-container">
-        <h2>Hello, nice to meet you! 👋🏼</h2>
-        <p>I’m Joanna Chang. I’m a researcher with a knack for user experience in digital environments and customer experience in offline retail settings. I’ve worked at Nielsen and CXG for five years. Currently, I’m pursuing a master’s degree in Media & Information at Michigan State University.</p>
-        <p>Feel free to check out my resume, and reach out if anything piques your interest.</p>
+    <div className="bg-gray-100 p-8">
+      <div style={{ paddingLeft: '4rem', paddingRight: '4rem', marginTop: '3em', marginBottom: '3em', marginLeft: 'auto', marginRight: 'auto', maxWidth: '800px' }}>
+        <Grid container spacing={3} alignItems="center">
+          <Grid item xs={12} sm={6} style={{ display: 'flex', justifyContent: 'center' }}>
+            <img src={headshotUrl} alt="Joanna Chang" style={{ maxWidth: '80%', height: 'auto' }} />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <div>
+              <h3 style={{ fontSize: '1.8rem', color: '#2B9348' }}>Hello, nice to meet you! 👋🏼</h3>
+              <p style={{ fontSize: '1rem' }}>I’m Joanna Chang. I’m a researcher with a knack for user experience in digital environments and customer experience in offline retail settings. I’ve worked at Nielsen and CXG for five years. Currently, I’m pursuing a master’s degree in Media & Information at Michigan State University.</p>
+              <p style={{ fontSize: '1rem' }}>Feel free to check out my resume, and reach out if anything piques your interest.</p>
+            </div>
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
--- src/Resume.js
+++ src/Resume.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { resumeEducation, resumeWork, resumeSkills } from './Supabase';
-import './Resume.css';
-import Button from '@mui/material/Button';
+import { Button, Typography, List, ListItem, ListItemText } from '@mui/material';
 
 function Resume() {
   const [schools, setSchools] = useState([]);
@@ -27,71 +26,63 @@ function Resume() {
 
   return (
     <div className="ResumeContainer">
-      <div className="Resume">
-        <h3>EDUCATION</h3>
-        {schools.map((school, index) => (
-          <div key={index} className="education-item">
-            <p className="university">
-              {school.university} ({school.university_short}) | {school.city}, {school.country}
-            </p>
-            <p className="degree">
-              {school.degree} in {school.school}
-            </p>
-            <p className="period">
-              {school.period}
-            </p>
-            {index !== schools.length - 1 && <hr />}
-          </div>
-        ))}
-        <h3>WORK EXPERIENCE</h3>
-        {works.sort((a, b) => a.id - b.id).map((work, index) => {
-          const isSameCompany = index > 0 && works[index - 1].company === work.company;
+      <div className="Resume" style={{ paddingLeft: '4rem', paddingRight: '4rem', marginTop: '4em', marginBottom: '4em' }}>
+        {/* EDUCATION SECTION */}
+        <Typography variant="h6" style={{ color: '#2B9348', marginTop: '0.8em' }} gutterBottom>EDUCATION</Typography>
+        <List>
+          {schools.map((school, index) => (
+            <ListItem key={index}>
+              <ListItemText
+                primary={`${school.university} (${school.university_short}) | ${school.city}, ${school.country}`}
+                secondary={`${school.degree} in ${school.school} | ${school.period}`}
+              />
+            </ListItem>
+          ))}
+        </List>
 
-          return (
-            <div key={index} className="work-item">
-              {index !== 0 && !isSameCompany && (
-                <hr />
-              )}
-              {index !== 0 && isSameCompany && (
-                <hr className="hr-dotted" />
-              )}
-              {!isSameCompany && (
-                <p className="first-line">
-                  <span className="company">{work.company}</span> | <span className="location">{work.location}</span>
-                </p>
-              )}
-              <p className="second-line">
-                {work.title}, {work.department}
-              </p>
-              <p className="period">
-                {work.period}
-              </p>
-            </div>
-          );
-        })}
-        <h3>SKILLS</h3>
-        {skills.reduce((acc, skill, index) => {
-          const prevSkill = index > 0 ? skills[index - 1] : null;
-          const isNewCategory = prevSkill ? prevSkill.category !== skill.category : true;
+        {/* WORK EXPERIENCE SECTION */}
+        <Typography variant="h6" style={{ color: '#2B9348', marginTop: '0.8em' }} gutterBottom>WORK EXPERIENCE</Typography>
+        <List>
+          {works.sort((a, b) => a.id - b.id).map((work, index) => {
+            const prevWork = works[index - 1];
+            const isSameCompany = index > 0 && prevWork.company === work.company;
 
-          if (!isNewCategory && index !== 0) {
-            acc[acc.length - 1] = (
-              <p key={index} className="first-line-skills">
-                {`${acc[acc.length - 1].props.children}, ${skill.name}`}
-              </p>
+            return (
+              <ListItem key={index}>
+                <ListItemText
+                  primary={isSameCompany ? null : `${work.company} | ${work.location}`}
+                  secondary={`${work.title}, ${work.department} | ${work.period}`}
+                />
+              </ListItem>
             );
-          } else {
-            acc.push(
-              <p key={index} className="first-line-skills">
-                {`${skill.category}: ${skill.name}`}
-              </p>
-            );
-          }
+          })}
+        </List>
 
-          return acc;
-        }, [])}
-        <div style={{ height: '40px' }}></div>
-        <div className="centered-button-container">
+        {/* SKILLS SECTION */}
+        <Typography variant="h6" style={{ color: '#2B9348', marginTop: '0.8em' }} gutterBottom>SKILLS</Typography>
+        <List>
+          {skills.reduce((acc, skill, index) => {
+            const prevSkill = index > 0 ? skills[index - 1] : null;
+            const isNewCategory = prevSkill ? prevSkill.category !== skill.category : true;
+
+            if (!isNewCategory && index !== 0) {
+              acc[acc.length - 1].secondary += `, ${skill.name}`;
+            } else {
+              acc.push({
+                primary: `${skill.category}:`,
+                secondary: `${skill.name}`
+              });
+            }
+
+            return acc;
+          }, []).map((skill, index) => (
+            <ListItem key={index}>
+              <ListItemText primary={skill.primary} secondary={skill.secondary} />
+            </ListItem>
+          ))}
+        </List>
+
+        <div className="centered-button-container" style={{ marginTop: '1.2em', display: 'flex', justifyContent: 'center' }}>
           <Button variant="contained" onClick={handleResumeButtonClick} style={{ backgroundColor: '#575761', color: 'white' }}>
             View Resume PDF
           </Button>
