afc3607df2c3e434fa4e5c5638770a57363fb9d9
joannacfc <chang111@msu.edu>
2024-04-20 23:12:54 -0400

    responsive modification

--- src/MainPage.js
+++ src/MainPage.js
@@ -4,28 +4,34 @@ import video_redesign from './video_redesign.gif';
 import { Link } from 'react-router-dom';
 
 function MainPage() {
-  const [color, setColor] = useState('#2B9348');
+  const [hovered, setHovered] = useState(false);
 
-  const handleClick = () => {
-    setColor('#575761');
+  const handleMouseEnter = () => {
+    setHovered(true);
   };
 
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
+
+  const color = hovered ? '#575761' : '#2B9348';
+
   return (
-    <div className="MainPage" style={{ paddingLeft: '4rem', paddingRight: '4rem', marginTop: '5em', marginBottom: '6em', marginLeft: 'auto', marginRight: 'auto', maxWidth: '960px' }}>
+    <div className="MainPage" style={{ paddingLeft: '4rem', paddingRight: '4rem', marginTop: '4em', marginBottom: '6em', marginLeft: 'auto', marginRight: 'auto', maxWidth: '960px' }}>
       <Grid container spacing={1} alignItems="flex-start">
-        <Grid item xs={4} sx={{ flexBasis: '25%' }}>
-          <div className="MainPage-content">
+        <Grid item xs={12} sm={8} sx={{ flexBasis: '75%' }}>
+          <div className="MainPage-video">
+            <img src={video_redesign} alt="Application Redesign" style={{ maxWidth: '100%', height: 'auto' }}/>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={4} sx={{ flexBasis: '25%' }}>
+          <div className="MainPage-content" style={{ marginTop: '2em'}}>
             <Link to="/Portfolio1" style={{ textDecoration: 'none' }}>
-              <h2 style={{ fontSize: '1.6rem', color: color }} onClick={handleClick}>Application Redesign</h2>
+              <h2 style={{ fontSize: '1.6rem', color: color }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Application Redesign</h2>
             </Link>
             <p style={{ fontSize: '1rem'}}>Enhancing User Experience (UX)</p>
             <p style={{ fontSize: '1rem'}}>Prototype Creation in Figma</p>
             <p style={{ fontSize: '1rem', marginBottom: '18rem'}}>2023</p>
-          </div>
-        </Grid>
-        <Grid item xs={8} sx={{ flexBasis: '75%' }}>
-          <div className="MainPage-video">
-            <img src={video_redesign} alt="Application Redesign" style={{ maxWidth: '100%', height: 'auto' }}/>
           </div>
         </Grid>
       </Grid>
